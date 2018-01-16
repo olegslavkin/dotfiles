d63559ab23d9d010bd85956682584bc8394d0fdc
Oleg Slavkin <oleg@slavkin.info>
2018-01-16 18:14:46 +0300

    Add functions many...

--- install.sh
+++ install.sh
@@ -4,23 +4,26 @@ SSHKEYGEN=0
 VIM_PLUG_INSTALL=1
 
 ## Functions
+function yesno() {
+	case "$1" in
+		[yY] | [yY][eE][sS]) return   1 ;;
+		[nN] | [nN][oO])     return   0 ;;
+		*)                   return 254 ;;
+	esac
+}
+
 function checkutils() {
-If [ -z $(which git) ]; then
-	echo "Please install git utils"
-fi
-if [ -z $(which curl) ]; then
-	echo "Please install curl"
-fi
+	local whichutil=$(which $1)
+	if [ -z $whichutil ]; then
+		echo -e "Please install $1"
+	fi
 }
 
 # Generate SSH key for github.com
 function sshkeygen() {
 read -p "Generate ssh-key for github.com [y/n]:" _sshkeygithub
-case "$_sshkeygithub" in
-        [yY] | [yY][eE][sS]) SSHKEYGEN=1 ;;
-        [nN] | [nN][oO])     SSHKEYGEN=0 ;;
-        *)                   SSHKEYGEN=0 ;;
-esac
+yesno $_sshkeygithub
+SSHKEYGEN=$?
 
 if [ $SSHKEYGEN -eq 1 ]; then
         read -sp "Please input the password for ssh-key:" _sshkeygithub_pass
@@ -30,18 +33,29 @@ fi
 }
 
 function vim_plug_install() {
+	local _vim_plug=0
 	read -p "Install vim-plug [y/n]:" _vim_plug_install
-	case "_vim_plug_install" in
-		[yY] | [yY][eE][sS]) VIM_PLUG_INSTALL=1 ;;
-		[nN] | [nN][oO])     VIM_PLUG_INSTALL=0 ;;
-		*)                   VIM_PLUG_INSTALL=0 ;;
-	esac
-	cd $HOME
-	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	yesno $_vim_plug_install
+	_vim_plug=$?
+	if [ $_vim_plug -eq 1 ]; then
+		checkutils _curl
+		cd $HOME
+		curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    		  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	fi
 }
 
-function vim_plug_postinstall() {
+function automount_vboxsf() {
+	# TODO
+	# Check host.
+	local isVirtualBox=$(lspci -vv | grep -i virtualbox | wc -l)
+	if [ $isVirtualBox -eq 0 ]; then
+		return -1
+	fi
+	read -p "Add automount vboxsf [y/n]:" _automount_vboxsf
+	read -p "Please input shared folder:" _vboxshared
+	read -p "Please input mount folder:" _mount_folder
+	sudo echo "$_vboxshared $_mount_folder vboxsf defaults,uid=$(id -u),gid=$(id -u),dmode=700,fmode=600 0 0" #>> /etc/fstab 
 }
 
 # Copy files
@@ -49,8 +63,17 @@ function copyfiles() {
 cp -r .tmux.conf tmux-keybindings.conf .ssh .gitconfig $HOME
 }
 
+function testyn() {
+	yesno $1
+	echo $?
+}
 
+
+###########################
 ## main
+##
+###########################
+
 #checkutils
 #sshkeygen
 #copyfiles
